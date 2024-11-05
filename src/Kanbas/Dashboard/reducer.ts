618f6e155f56f897658cbe4878029868cba6439d
Abhishek Raghuraman <abhishek.raghuraman0211@gmail.com>
2024-11-05 18:05:52 -0500

    Enrolments

--- src/Kanbas/Dashboard/reducer.ts
+++ src/Kanbas/Dashboard/reducer.ts
@@ -2,7 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 import { enrollments } from '../Database';
 const initialState = {
     enrollments: enrollments
-     
 };
 
 const enrollmentSlice = createSlice({

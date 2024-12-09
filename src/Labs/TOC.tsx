2e5eab071649e0007a6f8294dca4dd836efe4ad2
Abhishek Raghuraman <abhishek.raghuraman0211@gmail.com>
2024-12-09 17:14:48 -0500

    server link update

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -14,7 +14,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://kanbas-node-server-app-m012.onrender.com" className="nav-link">
+        <a id="wd-k" href="https://kanbas-node-server-app-mongo-8mjw.onrender.com/" className="nav-link">
           Server Deploy Link
         </a>
       </li>
